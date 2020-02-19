ad8544e94124346fb510607b48eab5e577d387f5
Daniel Lepe Vega <a01633986@itesm.mx>
2020-02-18 21:07:46 -0600

    Solve c-basics lab
    Signed-off-by: Daniel Lepe Vega <a01633986@itesm.mx>

--- labs/c-basics/fahrenheit_celsius.c
+++ labs/c-basics/fahrenheit_celsius.c
@@ -1,17 +1,23 @@
+#include <stdlib.h>
 #include <stdio.h>
-
-#define   LOWER  0       /* lower limit of table */
-#define   UPPER  300     /* upper limit */
-#define   STEP   20      /* step size */
 
 /* print Fahrenheit-Celsius table */
 
-int main()
+int main(int argc, char **argv)
 {
-    int fahr;
-
-    for (fahr = LOWER; fahr <= UPPER; fahr = fahr + STEP)
-	printf("Fahrenheit: %3d, Celcius: %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
-
-    return 0;
+        int fahr;
+        if(argc > 2){
+	    int start = atoi(argv[1]);
+	    int end = atoi(argv[2]);
+	    int steps = atoi(argv[3]);
+            for (fahr=start; fahr<=end; fahr=fahr+steps){
+                printf("Fahrenheit: %3d, Celcius: %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
+	    }
+        }
+        else{
+            float num_farenheit_degress = atof(argv[1]);
+            printf("Fahrenheit: %3f, Celcius: %6.1f\n", num_farenheit_degress, (num_farenheit_degress-32)*(5.0/9.0));
+        }    
+        return 0;
 }
+
