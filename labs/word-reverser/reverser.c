b515c49d2693345875c8ac76e4071b900fa81b5e
DanielLepeVega <danylepevega@gmail.com>
2020-03-03 16:43:52 -0600

    Solve word-reverser lab
    Signed-off-by: DanielLepeVega <danylepevega@gmail.com>

--- labs/word-reverser/reverser.c
+++ labs/word-reverser/reverser.c
@@ -1,6 +1,29 @@
 #include <stdio.h>
+void displayString(char str[]);
 
-int main(){
-    // Place your magic here
+int main(int argc, char *argv[])
+{
+    char str[50];
+    printf("Input: ");
+    char c = getchar();
+    int i = 0;
+    while(c != '\n'){
+        c = getchar();
+        str[i] = c;
+        if(c == '\n'){
+            break;
+        }
+    }             
+    displayString(str);     // Passing string to a function.    
     return 0;
+}
+void displayString(char str[])
+{
+    /*for(int k = 0;k<sizeof(str);k++){
+        printf("%c", str[k]);
+    }*/
+    printf("Reversed Output: ");
+    for(int i=50;i>0;i--){
+        printf("%c", str[i]);
+    }
 }
