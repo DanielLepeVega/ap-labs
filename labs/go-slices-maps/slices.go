a4704027cba4cc118834a923ab537d86465995ad
Daniel Lepe Vega <danylepevega@gmail.com>
2020-03-10 20:19:22 -0600

    Solved go-slices-maps
    Signed-off-by: Daniel Lepe Vega <danylepevega@gmail.com>

--- labs/go-slices-maps/slices.go
+++ labs/go-slices-maps/slices.go
@@ -1,12 +1,13 @@
-package main
+package main //final
 
 import "golang.org/x/tour/pic"
+
 func Pic(dx, dy int) [][]uint8 {
-	p := make([][]uint8,dy)
-	for i:=0;i<dy;i++ {
-		p[i] = make([]uint8,dx)
-		for j:=0;j<dx;j++ {
-			p[i][j] = uint8(i^j)
+	p := make([][]uint8, dy)
+	for i := 0; i < dy; i++ {
+		p[i] = make([]uint8, dx)
+		for j := 0; j < dx; j++ {
+			p[i][j] = uint8(i ^ j)
 		}
 	}
 	return p
