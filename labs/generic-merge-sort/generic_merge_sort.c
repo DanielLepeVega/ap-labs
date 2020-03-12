ec518dd7c1140981d8ce72cbfa7aaf0ac00d7291
Daniel Lepe Vega <danylepevega@gmail.com>
2020-03-12 08:19:42 -0600

    Avance generic-merge-sort
    Signed-off-by: Daniel Lepe Vega <danylepevega@gmail.com>

--- labs/generic-merge-sort/generic_merge_sort.c
+++ labs/generic-merge-sort/generic_merge_sort.c
@@ -3,73 +3,73 @@
 #include <string.h>
 
 int main()
-void mergeSort(void *v[], int left, int right,int (*comp)(void *, void *));
+void mergeSort(void *v[], int leftEdge, int rightEdge,int (*comp)(void *, void *));
 
-void mergeSort(void *v[], int left, int right,int (*comp)(void *, void *))
-{
-    void merge(void *v[], int left,int mid, int right,
-        int (*comp)(void *, void *));
-
-    int mid;
-    if (left >= right) /* do nothing if array contains */
-        return; /* fewer than two elements */
-    mid = ((right+left)/2);
-    mergeSort(v, left, mid, comp);
-    mergeSort(v, mid+1, right, comp);
-    merge(v, left, mid, right, comp);
-}
-
-void merge(void *v[], int left,int mid, int right,
-    int (*comp)(void *, void *)){
-
-    void** auxLeft = (void *) malloc((mid - left+1)*sizeof(void *));
-    void** auxRight= (void *) malloc((right - mid)*sizeof(void *));
-    int j=0;
-    for(int i =left ; i<=mid ; i++){
-        auxLeft[j] = v[i];
-        j++;
-    }
-    j=0;
-    for(int i =mid+1 ; i<=right ; i++){
-        auxRight[j] = v[i];
-        j++;
-    }
-    int l=0,r=0;
-    for(int i=left ; i<=right ; i++){
-        if(l > mid - left){
-            v[i] = auxRight[r];
-            r++;
-        }else if(r > right- (mid+1)){
-            v[i] = auxLeft[l];
-            l++;
-        }else{
-            int compare = ((* comp)( auxLeft[l], auxRight[r]));
-            if(compare>0){
-                //right goes first.
-                v[i] = auxRight[r];
-                r++;
-            }else{
-                //left goes first;
-                v[i] = auxLeft[l];
-                l++;
-            }
-        }
-    }
-    free(auxRight);
-    free(auxLeft);
-}
-int numcmp(char *s1, char *s2) {
+int numcmp(char *s1, char *s2){
     double val1, val2;
     val1 = atof(s1);
     val2 = atof(s2);
 
-    if(val1 < val2) {
+    if(val1 < val2){
         return -1;
     }
     else if(val1 > val2){
         return 1;
     }
     return 0;
+}
+
+void merge(void *v[], int leftEdge,int mid, int rightEdge,
+    int (*comp)(void *, void *)){
+
+    void** auxleftEdge = (void *) malloc((mid - leftEdge+1)*sizeof(void *));
+    void** auxrightEdge = (void *) malloc((rightEdge - mid)*sizeof(void *));
+    int j=0;
+    for(int i =leftEdge ; i<=mid ; i++){
+        auxleftEdge[j] = v[i];
+        j++;
+    }
+    j=0;
+    for(int i =mid+1 ; i<=rightEdge; i++){
+        auxrightEdge[j] = v[i];
+        j++;
+    }
+    int l=0,r=0;
+    for(int i=leftEdge ; i<=rightEdge; i++){
+        if(l > mid - leftEdge){
+            v[i] = auxrightEdge[r];
+            r++;
+        }else if(r > rightEdge - (mid+1)){
+            v[i] = auxleftEdge[l];
+            l++;
+        }else{
+            int compare = ((* comp)( auxleftEdge[l], auxrightEdge[r]));
+            if(compare>0){
+                //rightEdge goes first.
+                v[i] = auxrightEdge[r];
+                r++;
+            }else{
+                //leftEdge goes first;
+                v[i] = auxleftEdge[l];
+                l++;
+            }
+        }
+    }
+    free(auxrightEdge);
+    free(auxleftEdge);
+}
+
+void mergeSort(void *v[], int leftEdge, int rightEdge,int (*comp)(void *, void *))
+{
+    void merge(void *v[], int leftEdge,int mid, int rightEdge, int (*comp)(void *, void *));
+
+    int mid;
+    if (leftEdge >= rightEdge) /* do nothing if array contains */
+        return; /* fewer than two elements */
+    mid = ((rightEdge + leftEdge)/2);
+    mergeSort(v, leftEdge, mid, comp);
+    mergeSort(v, mid+1, rightEdge, comp);
+    merge(v, leftEdge, mid, rightEdge, comp);
 }
 
 int main(int argc, char** argv){
@@ -81,6 +81,7 @@ int main(int argc, char** argv){
         mergeSort((void *)argv, numericValue? 2 : 1, argc-1, (int(*)(void *, void *)) numericValue? numcmp:strcmp);
         for(int i=2;i<argc;i++){
             printf("%s ", argv[i]);
-        }printf("\n"); 
+        }
+        printf("\n"); 
     }   
 }
