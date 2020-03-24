78e74f5360ed7d90902f4d1ba60c26bf5f5f3bac
Daniel Lepe Vega <danylepevega@gmail.com>
2020-03-24 17:01:22 -0600

    str-lib final
    Signed-off-by: Daniel Lepe Vega <danylepevega@gmail.com>

--- labs/str-lib/main.c
+++ labs/str-lib/main.c
@@ -1,5 +1,24 @@
 #include <stdio.h>
 
-int main() {
-    return 0;
+int myStringLen(char *str);
+char *myStringAdd(char *origin, char *addition);
+int myStringFind(char *origin, char *substr);
+
+int main(int argc, char **argv) {
+	if(argc!=4){
+		printf("Only insert 3 arguments\n");
+		return -1;
+	}
+	printf("Initial Lenght: %d\n", myStringLen(argv[1]));
+	char *newStr = myStringAdd(argv[1],argv[2]);
+	printf("New String: %s\n", newStr);
+	int found = myStringFind(newStr,argv[3]);
+	printf("Substring was found: ");
+	if(found<0){
+		printf("No\n");
+	}
+	else{
+		printf("Yes, at pos %d\n", found);
+	}
+	return 0;
 }
--- labs/str-lib/strlib.c
+++ labs/str-lib/strlib.c
@@ -1,13 +1,36 @@
-
-
-int mystrlen(char *str){
-    return 0;
+#include <string.h>
+#include <stdlib.h>
+int myStringLen(char *str){
+	int i;
+	for(i=0;str[i]!='\0';i++);
+    return i;
 }
 
-char *mystradd(char *origin, char *addition){
-    return 0;
+char *myStringAdd(char *origin, char *addition){
+	int size1 = myStringLen(origin);
+	int size2 = myStringLen(addition);
+	char *newString = calloc(size1+size2+1, sizeof(char));
+	strcpy(newString,origin);
+	strcpy(newString+size1,addition);
+    return newString;
 }
 
-int mystrfind(char *origin, char *substr){
-    return 0;
+int myStringFind(char *origin, char *substr){
+	int i;
+    int j; 
+    int found;
+	int size1 = myStringLen(origin);
+	int size2 = myStringLen(substr);
+	for(i=0;i<size1;i++){
+		found = 1;
+		for(j=0;i+j<size1 && j<size2;j++){
+			if(origin[i+j] != substr[j]){
+				found = 0;
+				break;
+			}
+		}
+		if(found && j==size2)
+			return i;
+	}
+    return -1;
 }
