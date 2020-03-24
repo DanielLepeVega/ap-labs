d2a26d260f80c86c2aa79cd1c40237e1f16b2ebd
Daniel Lepe Vega <danylepevega@gmail.com>
2020-03-24 17:13:49 -0600

    mycat final
    Signed-off-by: Daniel Lepe Vega <danylepevega@gmail.com>

--- labs/mycat/cat2.c
+++ labs/mycat/cat2.c
@@ -1,16 +1,10 @@
-include <stdio.h>
+#include <stdio.h>
 #include <fcntl.h>
 #include <unistd.h>
 
-/* filecopy:  copy file ifp to file ofp */
-void filecopy(FILE *ifp, FILE *ofp)
-int filecopy(int ifp, int ofp)
+/* fileCopy:  copy file ifp to file ofp */
+int fileCopy(int ifp, int ofp)
 {
-    int c;
-
-    while ((c = getc(ifp)) != EOF)
-        putc(c, ofp);
-
     int n;
     char c[100];
     while ((n = read(ifp, c, sizeof(c))) > 0)
@@ -22,38 +16,24 @@ int filecopy(int ifp, int ofp)
 /* cat:  concatenate files, version 2 */
 int main(int argc, char *argv[])
 {
-    FILE *fp;
-    void filecopy(FILE *, FILE *);
     int fp;
-    int filecopy(int ifp, int ofp);
+    int fileCopy(int ifp, int ofp);
     char *prog = argv[0];   /* program name for errors */
 
     if (argc == 1)  /* no args; copy standard input */
-        filecopy(stdin, stdout);
-        filecopy(0, 1);
+        fileCopy(0, 1);
     else
         while (--argc > 0)
-            if ((fp = fopen(*++argv, "r")) == NULL) {
-                fprintf(stderr, "%s: can′t open %s\n",
-			prog, *argv);
             if ((fp = open(*++argv, O_RDONLY)) == -1) {
                 fprintf(stderr, "%s: can′t open %s\n", prog, *argv);
                 return 1;
             } else {
-                filecopy(fp, stdout);
-                fclose(fp);
-                if(filecopy(fp, 1) < 0){
+                if(fileCopy(fp, 1) < 0){
                     fprintf(stderr, "%s: error writing stdout\n", prog);
                     close(fp);
                     return 2;
                 }
                 close(fp);
             }
-
-    if (ferror(stdout)) {
-        fprintf(stderr, "%s: error writing stdout\n", prog);
-        return 2;
-    }
-
     return 0;
 }
