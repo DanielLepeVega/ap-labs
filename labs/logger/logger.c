e81a62a93f49fcf08d496af21f88082ec8b8754c
DanielLepeVega <danylepevega@gmail.com>
2020-04-14 19:01:18 -0500

    Solved logger
    Signed-off-by: DanielLepeVega <danylepevega@gmail.com>

--- labs/logger/logger.c
+++ labs/logger/logger.c
@@ -1,41 +1,64 @@
 #include <stdio.h>
 #include <stdarg.h>
 #include <stdlib.h>
+#include <time.h>
 #include "logger.h"
 
 int myMethVPrintF(const char *format, va_list arg){
+	fflush(stdin); 
+	actualTime();
 	int r = vprintf(format, arg);
-	printf("\033[0m"); // Reset color
+	printf("\033[0m"); // resetColor
 	va_end(arg);
 	return r;
 }
 
 int infof(const char *format, ...){
-	printf("\033[0m"); // Reset color
+	fflush(stdin);
+	actualTime(); 
+	printf("\033[0m"); // resetColor
 	va_list arg;
 	va_start(arg, format);
 	return myMethVPrintF(format, arg);
 }
 
 int warnf(const char *format, ...){
-	printf("\033[0;33m"); // Set color to yellow
+	fflush(stdin);
+	actualTime(); 
+	printf("\033[0;33m"); // setColorToYellow
 	va_list arg;
 	va_start(arg, format);
 	return myMethVPrintF(format, arg);
 }
 
 int errorf(const char *format, ...){
-	printf("\033[0;31m"); // Set color red
+	fflush(stdin);
+	actualTime(); 
+	printf("\033[0;31m"); // setColorRed
 	va_list arg;
 	va_start(arg, format);
 	return myMethVPrintF(format, arg);
 }
 
 int panicf(const char *format, ...){
-	printf("\033[1;35m"); // Set color to bald magenta
+	fflush(stdin);
+	actualTime(); 
+	printf("\033[1;35m"); // setColorToBaldMagenta
 	va_list arg;
 	va_start(arg, format);
 	myMethVPrintF(format, arg);
 	abort();
 	return 0;
+}
+
+int actualTime ()
+{
+  time_t rawtime;
+  struct tm * timeinfo;
+
+  time ( &rawtime );
+  timeinfo = localtime ( &rawtime );
+  printf ( "Current local time and date: %s", asctime (timeinfo) );
+
+  return 0;
 }
